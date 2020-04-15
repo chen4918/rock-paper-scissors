a94c00321e48449425d862d677ac9db95ca99ee1
David Chen <chen4918@gmail.com>
2020-04-15 13:55:53 -0700

    Added UI and intial commit of refactored script

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+let roundResult;
+let playerSelection;
+let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
-let roundResult;
-let roundNumber = 0;
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const playerSpan = document.querySelector('#playerSpan');
+const computerSpan = document.querySelector('#computerSpan');
+const playerSelectText = document.querySelector('#playerSelect');
+const computerSelectText = document.querySelector('#computerSelect');
+const roundResultText = document.querySelector('.roundResult');
+const finalResultText = document.querySelector('.finalResult');
 
 function computerPlay() {
   let result = Math.random();
@@ -16,20 +26,14 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  let choice = prompt('Round ' + roundNumber + '\n\nRock, paper, or scissors?');
-  playerSelection = choice.toLowerCase();
   computerSelection = computerPlay();
+  playerSelectText.textContent = 'The player chose ' + playerSelection + '.';
+  computerSelectText.textContent = 'The computer chose ' + computerSelection + '.';
 
-  console.log('The player chose ' + playerSelection + '\. The computer chose ' + computerSelection + '\.');
-
-  if ((playerSelection !== 'rock') && (playerSelection !== 'paper') && (playerSelection !== 'scissors')) {
-    console.log('The player didn\'t enter a valid choice. Prompting them again...');
-    alert('Please enter a valid choice (rock, paper, or scissors).');
-    return playRound();
-    } else if (playerSelection === computerSelection) {
-      roundResult = 'Draw!';
-      return;
-    }
+  if (playerSelection === computerSelection) {
+    roundResult = 'Draw!';
+    return;
+  }
 
   if (playerSelection == 'rock') {
     if (computerSelection == 'paper') {
@@ -67,25 +71,29 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-  for (i = 1; i < 6; i++) {
-    roundNumber++;
-    playRound();
+  playRound();
+  playerSpan.textContent = playerScore;
+  computerSpan.textContent = computerScore;
+  roundResultText.textContent = roundResult;
 
-    console.log(roundResult);
-
-    if (i === 5) {
-      if (playerScore > computerScore) {
-        console.log('You win! The final score was ' + playerScore + ' to ' + computerScore + '\.');
-        return;
-      } else if (playerScore < computerScore) {
-        console.log('Sorry, you lost. The final score was ' + playerScore + ' to ' + computerScore + '\.');
-        return;
-      } else {
-        console.log('The game is a tie! The final score was ' + playerScore + ' to ' + computerScore + '\.');
-        return;
-      }
-    }
+  if (playerScore === 5) {
+    finalResultText.textContent = "YOU WIN!";
+  } else if (computerScore === 5) {
+    finalResultText.textContent = "YOU LOSE!";
   }
 }
 
-game();
+rock.addEventListener('click', (e) => {
+  playerSelection = 'rock';
+  game();
+})
+
+paper.addEventListener('click', (e) => {
+  playerSelection = 'paper';
+  game();
+})
+
+scissors.addEventListener('click', (e) => {
+  playerSelection = 'scissors';
+  game();
+})
